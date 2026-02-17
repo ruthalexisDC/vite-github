1649eff5799ab9192c50c7121f5d0cbc21bc4138
Ruth.A <ruthalexisdc.9@gmail.com>
2026-02-17 20:04:00 +0900

    updated vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig} from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig ({
-    base: "/vite-github/",
-})
+export default defineConfig({
+  base: "/vite-github/",
+});
